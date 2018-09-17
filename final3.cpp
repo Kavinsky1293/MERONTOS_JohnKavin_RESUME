772bc2e5fac44332e23dd7fba67de29ba94bdc3f
Kavinsky1293 <johndvin05@gmail.com>
2018-09-17 22:43:12 +0800

    css file for finalize resume

---
+++ final3.cpp
@@ -0,0 +1,109 @@
+<html>
+  <head>
+    <title>Professional Resume</title>
+    
+   <body background="newbackground.jpg";>
+    <div>
+   <img src="Profile Pic.jpg" alt="avatar" class="avatar" height="234" width="164"></div>
+  <style>
+  img {
+    border-radius: 50% ;
+    float: left;
+   }
+   p{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    left:100px;
+   }
+  div{
+    width: 1200px;
+    margin: auto;
+  
+    
+  }
+ </style>
+</body>
+</head>
+    </div>
+         <div style ="text-align: center">
+        <div style ="background-color: mintcream">
+        <font face = "Verdana" size = "14" color="darkgreen"><b>John Kavin R. Merontos<b></font><br />
+      
+        <br>
+          Brgy. 37-D Trading Boulevard, Davao City, Davao del Sur<br>
+       
+        <a href="johndvin05@gmail.com">johndvin05@gmail.com</a><br>
+      </div><hr>
+      </div>
+         <div>
+            <div style ="background-color: mintcream">
+             <font face = "Verdana" size = "6" color= "MediumSeaGreen ";><b>Objectives:<b></font><br />
+         
+        
+          <p>Secure an internship position for summer 2018 and that will allow me to utilize my  knowledge and experience  to ensure company's success.</p>
+        </div><hr>
+        <div>
+            <div style ="background-color: mintcream">
+            <div><img src= "educicon.png" height="70" style="float: left"/></div> <br>
+            
+             <font face = "Verdana" size = "6" color= "MediumSeaGreen ";>Educational Background:</font><br>
+ <p>
+Bachelor of Science of Information Technology&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fourth Year Level <br>
+Institute of Computing<br>
+University of Southeastern Philippines - Obrero<br>
+263 Iñigo St, Obrero, Davao City, Davao del Sur<br><br>
+</p>
+      
+            
+          </div><hr>
+         <div>
+            <div style ="background-color: mintcream">
+              <img src ="skills icon.png" height="70"/>
+             <font face = "Verdana" size = "6" color= "MediumSeaGreen ";>Skills:</font><br />
+    
+            <p><br>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Proficient in using Microsoft Office Apllication(Word, Excel,Powerpoint)<br>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Experience in Animation (Adobe Flash)<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Experience in Web Development and Database (HTML,CSS,Php, MYSQL)<br>
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Knowledge in creating a website/E-commerce <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Embodies punctuality at all times<br>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Can do multi-tasking skills<br>
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Can work well with other people<br><br>
+</p>
+            </ul>
+          </div><hr>
+        <div>
+            <div style ="background-color: mintcream">
+              <img src ="activitiesicon.png" height="70"/>
+             <font face = "Verdana" size = "6" color= "MediumSeaGreen ";>Activities:</font><br>
+      
+      <p><br>
+       Mindanao Conference of Information Technology Students (MCITS)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;December 03,2010</pre><br>
+        Philippine Society of Information Technology Educators (PSITE-XI)<br>
+        UIC Main Campus, Father Selga St., Davao City. <br><br>
+       Participant, Graphic Design Conference (Graphicon) 2016&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;December 10,2016<br>
+          The Davao United Association of Animators and Designers (DUAAD)<br>
+          CAP Auditorium , 37 Iñigo St, Poblacion District, Davao City<br><br>
+        </p>
+ 
+         </div><hr>
+          <div>
+           <div style ="background-color: mintcream">
+             <img src ="profreference+icon.png" height="70"/>
+             <font face = "Verdana" size = "6" color= "MediumSeaGreen ";>Professional Reference:</font><br />
+          <body>
+          <p>
+          Dr. Val A. Quimno<br>
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Program Head, Institute of Computing<br>
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;University of Southeastern Philippines<br>
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;227-8192 loc 276<br><br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Engr. Jamal Kay B. Rogers<br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Research Head, Institute of Computing<br>
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;University of Southeastern Philippines<br>
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;227-8192 loc 276<br>
+        </p>
+      </body>
+    </div>
+        </body>
+  </body>
+</html>
